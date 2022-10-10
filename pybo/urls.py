2974b881259f681eb87d7cdf9fe984e7e7af45b0
25th-Night <brown_eyed87@naver.com>
2022-10-10 10:15:44 +0900

    feat : create 질문 등록, 답변 등록
    1. static
     - 별도 폴더 관리 설정
    2. bootstrap
     - 부트스트랩 설치 및 적용
    3. template 상속
     - 표준 HTML 구조
     - base.html 템플릿
     - extend, block/endblock
    4. form
     - form 생성 및 view 연결

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from pybo.views import index, detail, answer_create
+from pybo.views import index, detail, question_create, answer_create
 
 
 app_name = "pybo"
 
 urlpatterns = [
-    path("", index),
+    path("", index, name="index"),
     path("<int:question_id>/", detail, name="detail"),
+    path("question/create/", question_create, name="question_create"),
     path("answer/create/<int:question_id>/", answer_create, name="answer_create"),
 ]
